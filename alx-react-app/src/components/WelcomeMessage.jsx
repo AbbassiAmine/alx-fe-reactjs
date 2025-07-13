df8ffa0b2c9694b059473a441b87a3033491ef22
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-07-13 21:49:55 +0100

    init

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>I am learning about JSX!</h1>
+            <p>This is a simple JSX component.</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
