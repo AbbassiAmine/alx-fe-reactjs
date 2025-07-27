51fe25e5316f73ffcecade59f03ea40bb38fa776
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-07-27 22:26:01 +0100

    added props

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 // Create a Context with an optional default value
-const MyContext = React.createContext();
+const UserContext = React.createContext();
 
 // Example with a specific default value
 const ThemeContext = React.createContext({
     theme: 'light',
     toggleTheme: () => { }
 });
+
+export default UserContext;
