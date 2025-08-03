f9711854d3b558937cb08947052088ce9f6e30c3
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-08-03 20:15:29 +0100

    recipe app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Recipe App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
