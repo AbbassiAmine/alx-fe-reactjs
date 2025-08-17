e1a13cfa1025ee8b5372fe6bd1dde99a7f19d2ae
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-08-17 20:42:46 +0100

    tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-        <div className="user-profile bg-gray-100 sm:p-4 md:p-8 xs:max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
             <img className="rounded-full  mx-auto  sm:w-24 h-24 md:w-36 h-36" src="https://via.placeholder.com/150" alt="User" />
             <h1 className="text-xl sm:text-sm md:text-base text-blue-800 my-4">John Doe</h1>
             <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
