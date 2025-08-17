5883fa0b088bccad2e57e04ffb0ddf7954ceb5b9
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-08-17 20:37:32 +0100

    tailwindcss

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import UserProfile from './components/UserProfile'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <UserProfile></UserProfile>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
