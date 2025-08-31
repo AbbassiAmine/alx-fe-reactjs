6d391c40e65b438f38b2e758bd3874e8ae588a04
Amine Abbassi <Contact.amine.abbassi@gmail.com>
2025-08-31 20:36:20 +0100

    posts app

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import PostsComponent from './components/PostsComponent';
 import Home from './components/Home';
+import Profile from './components/Profile';
 
 const queryClient = new QueryClient();
 
@@ -15,11 +16,13 @@ function App() {
             <ul style={{ listStyle: 'none', display: 'flex', gap: '20px' }}>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/posts">Posts</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
             </ul>
           </nav>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/posts" element={<PostsComponent />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
       </BrowserRouter>
